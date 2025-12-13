b8343a589964a61c261d9d4cf4b162daa97392be
Vitaliy Kataev <vita@kataev.pro>
2025-12-13 23:04:33 +0400

    feat: git commit sign

--- home/features/cli/git.nix
+++ home/features/cli/git.nix
@@ -3,8 +3,13 @@
     enable = true;
 
     settings = {
-      user.name = "Vitalii Kataev";
+      user.name = "Vitaliy Kataev";
       user.email = "vita@kataev.pro";
+
+      signing = {
+        key = "6664158A96D5F5D9DB8CD5DDBB28505EC0F75404";
+        signByDefault = true;
+      };
     };
   };
 }
